ab6e640ad38a40cfe75d84d4f1c4961134ef920c
alon dayan <alond9990@gmail.com>
2018-09-09 13:03:58 +0300

    added resource routers

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var groupsRouter = require('./routes/groups');
+var tasksRouter = require('./routes/tasks');
+var taskListsRouter = require('./routes/task_lists');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/groups', groupsRouter);
+app.use('/tasks', tasksRouter);
+app.use('/task_lists', taskListsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
