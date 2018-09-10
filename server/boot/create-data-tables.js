213b921a98f455fc4b59a5336b19436faff0d488
alon dayan <alond9990@gmail.com>
2018-09-10 04:35:04 +0300

    changed boot migrations file to include all models

--- server/boot/create-data-tables.js
+++ server/boot/create-data-tables.js
@@ -2,31 +2,37 @@
  * Created by alond9990 on 09/09/2018.
  */
 
-module.exports = function(app) {
-  app.dataSources.db.automigrate('TaskList', function(err) {
-    if (err) throw err;
-
-    app.models.TaskList.create([{
-      name: 'TODO List 1'
-    }], function(err, TaskList) {
-      if (err) throw err;
-
-      console.log('Models created: \n', TaskList);
-    });
-  });
-
-  app.dataSources.db.automigrate('Task', function(err) {
-    if (err) throw err;
-
-    app.models.Task.create([{
-      title: 'Task 1'
-    }, {
-      title: 'Task 2'
-    }], function(err, Task) {
-      if (err) throw err;
-
-      console.log('Models created: \n', Task);
-    });
-  });
-
+module.exports = function (app) {
+  app.dataSources.db.automigrate();
+  console.log("Performed automigration.");
 };
+
+
+//module.exports = function(app) {
+//  app.dataSources.db.automigrate('TaskList', function(err) {
+//    if (err) throw err;
+//
+//    app.models.TaskList.create([{
+//      name: 'TODO List 1'
+//    }], function(err, TaskList) {
+//      if (err) throw err;
+//
+//      console.log('Models created: \n', TaskList);
+//    });
+//  });
+//
+//  app.dataSources.db.automigrate('Task', function(err) {
+//    if (err) throw err;
+//
+//    app.models.Task.create([{
+//      title: 'Task 1'
+//    }, {
+//      title: 'Task 2'
+//    }], function(err, Task) {
+//      if (err) throw err;
+//
+//      console.log('Models created: \n', Task);
+//    });
+//  });
+//
+//};
