1f3bb9c9518f014d4af75c9e87a5ba684e4d14a8
alon dayan <alond9990@gmail.com>
2018-09-11 14:53:46 +0300

    added users to default get request for groups

--- common/models/task-list.js
+++ common/models/task-list.js
@@ -5,7 +5,7 @@ module.exports = function(Tasklist) {
   Tasklist.observe('before save', function createAndRelateGroup(ctx, next) {
     // only if a new list is created
     if (!ctx.instance) {
-      ctx.instance.authorized_group = new Date();
+      ctx.instance.authorized_group = '';
     }
     next();
   });
