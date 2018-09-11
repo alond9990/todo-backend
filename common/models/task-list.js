5f08d6910dcc5b5dac8379c2519138a13bde61fa
alon dayan <alond9990@gmail.com>
2018-09-11 16:26:17 +0300

    create new group at new task list creation

--- common/models/task-list.js
+++ common/models/task-list.js
@@ -4,10 +4,19 @@ module.exports = function(Tasklist) {
 
   Tasklist.observe('before save', function createAndRelateGroup(ctx, next) {
     // only if a new list is created
-    if (!ctx.instance) {
-      ctx.instance.authorized_group = '';
+    if (ctx.isNewInstance) {
+      var Group = Tasklist.app.models.Group;
+      Group.create({}, function(err, obj) {
+        var instanceOrData = ctx.data ? 'data' : 'instance';
+        ctx[instanceOrData].authorized_group = obj.id;
+      });
     }
     next();
   });
+
+  //Tasklist.observe('access', function limitToGroupUsers(ctx, next) {
+  //  ctx.query.where.authorized_group = ;
+  //  next();
+  //});
 
 };
