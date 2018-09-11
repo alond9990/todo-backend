9476d8d422ea2173d947781d6780f4312d52a4f9
alon dayan <alond9990@gmail.com>
2018-09-11 14:52:13 +0300

    added tasks to deault get request for task lists

--- common/models/task-list.js
+++ common/models/task-list.js
@@ -2,4 +2,12 @@
 
 module.exports = function(Tasklist) {
 
+  Tasklist.observe('before save', function createAndRelateGroup(ctx, next) {
+    // only if a new list is created
+    if (!ctx.instance) {
+      ctx.instance.authorized_group = new Date();
+    }
+    next();
+  });
+
 };
