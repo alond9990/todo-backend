bfbaad143843c8cd52ba60df06a20516405358a8
alon dayan <alond9990@gmail.com>
2018-09-12 20:46:14 +0300

    added resource routing

--- routes/task_lists.js
+++ routes/task_lists.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* CREATE task lists listing. */
+router.post('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 module.exports = router;
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET tasks listing. */
-router.get('/', function(req, res, next) {
+/* CREATE tasks listing. */
+router.post('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* Mark task as done */
+router.put('/done', function(req, res, next) {
   res.send('respond with a resource');
 });
 
