cd869aabf6ed9fd4fa60487d5e978c7675b7e668
alon dayan <alond9990@gmail.com>
2018-09-12 21:36:42 +0300

    created user get by credentials view

--- data_access/users.js
+++ data_access/users.js
@@ -6,6 +6,19 @@ var connection = require('./mysql_connection');
 
 function User() {
 
+    // get specific user by credentials
+    this.getUserByCredentials = function (username, password) {
+        // initialize database connection
+        connection.init();
+        connection.acquire(function (err, con) {
+            con.query("SELECT * FROM user WHERE username = '" + username + "' AND password = '" + password + "'",
+                function (err, result) {
+                    con.release();
+                    console.log(result);
+                    return result;
+                });
+        });
+    };
 
 }
 
