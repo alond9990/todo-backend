e728befa5326ced9d4a59c00464a3b9049d2c5aa
alon dayan <alond9990@gmail.com>
2018-09-14 22:02:15 +0300

    hash user password

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const user = require('../data_access/users');
+const bcrypt = require('bcrypt');
 
 /* GET home page */
 router.get('/', function(req, res, next) {
@@ -16,7 +17,7 @@ router.post('/login', async function(req, res, next) {
             if (user) {
                 res.send(user);
             } else {
-                res.status(404).send({"error": "No user matching credentials."});
+                res.status(401).send({"error": "No user matching credentials."});
             }
         });
 });
@@ -25,6 +26,13 @@ router.post('/login', async function(req, res, next) {
 router.post('/register', async function(req, res, next) {
     let username = req.body.username;
     let password = req.body.password;
+    // hash the password
+    bcrypt.hash(password, 10, function(err, hash) {
+        if(err) {
+            return res.status(400).send({"error": err})
+        }
+        password = hash;
+    });
     try {
         let user = await user.createNewUser(username, password);
         res.send(user);
