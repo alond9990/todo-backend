4806a24d6247292a95db67d32654ef346b39b58c
alon dayan <alond9990@gmail.com>
2018-09-15 18:23:42 +0300

    added validations for task routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const task = require('../data_access/tasks');
 
+const expressJoi = require('express-joi-validator');
+const validations = require('./validations/task');
+
 /* CREATE task listing. */
-router.post('/', async function(req, res, next) {
+router.post('/', expressJoi(validations.create), async function(req, res, next) {
     let title = req.body.title;
     let taskListId = req.body.taskListId;
     let response = await task.createTask(title, taskListId);
@@ -11,7 +14,7 @@ router.post('/', async function(req, res, next) {
 });
 
 /* Mark task as done */
-router.put('/:id/done', async function(req, res, next) {
+router.put('/:id/done', expressJoi(validations.markAsDone), async function(req, res, next) {
     let taskId = req.params.id;
     let response = await task.markTaskAsDone(taskId);
     res.send(response);
